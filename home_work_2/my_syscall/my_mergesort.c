207556e4e074cfdd9e77a9e118249822133ad47f
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-02-08 21:13:45 -0700

    updating my_mergersort.c file

--- home_work_2/my_syscall/my_mergesort.c
+++ home_work_2/my_syscall/my_mergesort.c
@@ -4,14 +4,37 @@
 #include <unistd.h>
 
 #define MY_SYSCALL_NUMBER			398
+#define MY_MEREGESORT_NUMBER		399
+
+#define ARRAY_SIZE					10
 
 int main(void){
 
+	int fromSyscall = 0, array_index = 0;
 
-	int fromSyscall = 0;
+	int *pUnsortedArray = (int*)malloc(sizeof(int) * ARRAY_SIZE);
+	int *pSortedArray = (int*)malloc(sizeof(int) * ARRAY_SIZE);
+ 
+ 	printf("Unsorted array is:");
+    for(int i = 0; i < ARRAY_SIZE; i++)
+    {
+        *(pUnsortedArray + i) = rand();
+        printf("%d ", *(pUnsortedArray + i));
+    }
 
-	printf("%d\n", syscall(MY_SYSCALL_NUMBER));
+    if(syscall(MY_MEREGESORT_NUMBER) == 0){
+    	printf("\nSorted array is \n");
+    	for(int i = 0; i < ARRAY_SIZE; i++)
+    	{
+    	    printf("%d ", *(pUnsortedArray + i));
+ 	    }
+	}
+	else
+	{
+		printf("Error from system call\n");
+	}
 
-	return 0;
+    return 0;
 
 }
+
