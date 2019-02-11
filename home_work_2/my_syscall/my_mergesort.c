c04b4ac356899ed035d626ae8cf45a1fd750225d
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-02-10 23:02:13 -0700

    updating my_mergesort.c file to handle test cases

--- home_work_2/my_syscall/my_mergesort.c
+++ home_work_2/my_syscall/my_mergesort.c
@@ -4,10 +4,10 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define MY_SYSCALL_NUMBER			398
-#define MY_MEREGESORT_NUMBER		399
+#define MY_SYSCALL_NUMBER           398
+#define MY_MEREGESORT_NUMBER        399
 
-#define ARRAY_SIZE					256
+#define ARRAY_SIZE                  256
 
 //#define TEST_INVALID_SYS_CALL_NUMBER
 //#define TEST_INVALID_ADDRESS
@@ -16,7 +16,7 @@
 
 int main(void){
 
-	int32_t fromSyscall = 0, array_index = 0;
+    int32_t fromSyscall = 0, array_index = 0;
     int32_t myArraySize = ARRAY_SIZE;
 
     /*Check for valid array size. Exit if size is invalid*/
@@ -26,20 +26,20 @@ int main(void){
     }
 
     /*Allocate memory to unsorted array if unable to allocate then exit*/
-	int32_t *pUnsortedArray = (int32_t*)malloc(sizeof(int32_t) * ARRAY_SIZE);
+    int32_t *pUnsortedArray = (int32_t*)malloc(sizeof(int32_t) * ARRAY_SIZE);
     if(pUnsortedArray == NULL){
         printf("USER SPACE: Unable to allocate memory to pUnsortedArray\n");
         exit(0);
     }
 
     /*Allocate memory to sorted array if unable to allocate then exit*/
-	int32_t *pSortedArray = (int32_t*)malloc(sizeof(int32_t) * ARRAY_SIZE);
+    int32_t *pSortedArray = (int32_t*)malloc(sizeof(int32_t) * ARRAY_SIZE);
     if(pUnsortedArray == NULL){
         printf("USER SPACE: Unable to allocate memory to pUnsortedArray\n");
         exit(0);
     }
 
- 	printf("*** USER SPACE: Unsorted array ***\n");
+    printf("*** USER SPACE: Unsorted array ***\n");
     for(int32_t i = 0; i < ARRAY_SIZE; i++)
     {
         *(pUnsortedArray + i) = rand() % 1000 + 1;
@@ -66,26 +66,27 @@ int main(void){
 
     #ifdef TEST_MULTTIPLE_CALLS
         /*Call to system all my_mergesort*/
-        for(int32_t i = 0; i < 3; i++)
+        for(int32_t i = 0; i < 10; i++)
             fromSyscall = syscall(MY_MEREGESORT_NUMBER, pUnsortedArray, pSortedArray, myArraySize);
     #endif
 
+    /*Normal one time execution*/
     fromSyscall = syscall(MY_MEREGESORT_NUMBER, pUnsortedArray, pSortedArray, myArraySize);    
 
 
     if(fromSyscall == 0){
         printf("\nUSER SPACE: Sucessfully sorted array\n");   
-    	printf("*** USER SPACE: Sorted array ***\n");
-    	for(int32_t i = 0; i < ARRAY_SIZE; i++)
-    	{
-    	    printf("%d ", *(pSortedArray + i));
- 	    }
-	}
-	else
-	{
+        printf("*** USER SPACE: Sorted array ***\n");
+        for(int32_t i = 0; i < ARRAY_SIZE; i++)
+        {
+            printf("%d ", *(pSortedArray + i));
+        }
+    }
+    else
+    {
         printf("USER SPACE: Array is not sorted\n");
-		printf("USER SAPCE: Error from system call\n ERRRO NUMBER :%x\n return value : %d\n", -EFAULT, fromSyscall);
-	}
+        printf("USER SAPCE: Error from system call\n ERRRO NUMBER :%x\n return value : %d\n", -EFAULT, fromSyscall);
+    }
 
     /*free allocated memory to unsorted and sorted array*/
     free(pUnsortedArray);
