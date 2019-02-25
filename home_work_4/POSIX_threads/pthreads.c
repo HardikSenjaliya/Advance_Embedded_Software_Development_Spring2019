eb42e8d561b8075f834104d18556e1c2a49262ee
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-02-24 19:11:23 -0700

    updating end of file check condition for thread0 and adding timestamp to logfile

--- home_work_4/POSIX_threads/pthreads.c
+++ home_work_4/POSIX_threads/pthreads.c
@@ -1,3 +1,11 @@
+/*
+ * threads.c
+ *
+ *  Created on: Feb 23, 2019
+ *      Author: hardyk
+ */
+
+
 #include <stdio.h>
 #include <time.h>
 #include <pthread.h>
@@ -9,27 +17,39 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 
+
+//#define NSEC_PER_SEC (1000000000)
+//#define NSEC_PER_MSEC (1000000)
+#define NSEC_PER_MICROSEC 					(1000)
 #define	NUMBER_OF_THREADS 					(2)
 #define SIZE_ROW							(256)
 #define SIZE_COL							(2)
+#define MDT									(-7)
 
 /*Function prototype*/
 void readMapChars(FILE *fp, int array[SIZE_ROW][SIZE_COL]);
 
-pthread_mutex_t mFile;
+/*Mutex for synchronized operation to write into logfile*/
+pthread_mutex_t mFile, mTime;
 
+/*Structure to pass data for thread function*/
 typedef struct threadData
 {
-	char *fileName;
-	int threadId;
+	char *fileName;		/*log file name*/
+
 }threadData_t;
 
+/*global: file pointer for a logfile*/
 FILE *pLogFile;
+
+/*global: clock/time related variables*/
+time_t currentTime;
+struct tm *cTime;
 
 void readMapChars(FILE *fp, int array[SIZE_ROW][SIZE_COL]){
 
-	char c; 
-	while((c = fgetc(fp)) != EOF){
+	char c;
+	while((c = fgetc(fp)) != EOF  && !feof(fp)){
 		if((c >= 65) || (c <= 90))
 			c = tolower(c);
 
@@ -38,10 +58,13 @@ void readMapChars(FILE *fp, int array[SIZE_ROW][SIZE_COL]){
 	}
 }
 
-
-
 void *funThread0(void *arg){
-	
+
+	/*Get an exclusive access of a global clock/time variabels*/
+	pthread_mutex_lock(&mTime);
+	time(&currentTime);
+	cTime = gmtime(&currentTime);
+
 	threadData_t *data = (threadData_t*)(arg);
 	int mapChars[SIZE_ROW][SIZE_COL];
 	int indexR = 0, indexC = 0;
@@ -53,12 +76,12 @@ void *funThread0(void *arg){
 	}
 
 	/*Read values of posixID and LinuxID of a thread*/
-	long int posixID = pthread_self();
-	int linuxID = syscall(SYS_gettid);
+	pthread_t posixID = pthread_self();
+	pid_t linuxID = syscall(SYS_gettid);
 
-	printf("thread 0 %ld and %d \n", posixID, linuxID);
+	printf("thread0 %lu and %d \n", posixID, linuxID);
 
-	/*Get the exclusive access of a logfile*/
+	/*Get an exclusive access of a logfile*/
 	pthread_mutex_lock(&mFile);
 
 	/*Open the logfile*/
@@ -67,11 +90,12 @@ void *funThread0(void *arg){
 		printf("Unable to open log file\n");
 
 	/*Write to the log file*/
-	fprintf(pLogFile, "Thread 0 is writing to the file\n");
-	fprintf(pLogFile, "POSIX id is %ld and LINUX id is %d\n", posixID, linuxID);
+	fprintf(pLogFile, "Thread0 is writing to the file\n");
+	fprintf(pLogFile, "Thread0 Starting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
+	fprintf(pLogFile, "Thread0 POSIX id is %lu and LINUX id is %d\n", posixID, linuxID);
 
 	/*Open the test file to read data*/
-	FILE *pTestFile = fopen("gdb.txt", "r");
+	FILE *pTestFile = fopen("/usr/bin/gdb.txt", "r");
 	if(pTestFile == NULL)
 		printf("Unable to open test file\n");
 
@@ -88,35 +112,52 @@ void *funThread0(void *arg){
 			fprintf(pLogFile,"%c\t %d\t\n", mapChars[indexR][0], mapChars[indexR][1]);
 	}
 
+	time(&currentTime);
+	cTime = gmtime(&currentTime);
+	fprintf(pLogFile, "Thread0 Exiting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
+
 	fclose(pLogFile);
 
+	pthread_mutex_unlock(&mTime);
 	pthread_mutex_unlock(&mFile);
 
 	return NULL;
 }
 
 
-
-
 void *funThread1(void *arg){
+
+	/*Get an exclusive access of a global clock/time variabels*/
+	pthread_mutex_lock(&mTime);
+	time(&currentTime);
+	cTime = gmtime(&currentTime);
 
 	threadData_t *data = (threadData_t*)(arg);
 
-	long int posixID = pthread_self();
-	int linuxID = syscall(SYS_gettid);
+	pthread_t posixID = pthread_self();
+	pid_t linuxID = syscall(SYS_gettid);
 
-	printf("thread 1 %ld and %d \n", posixID, linuxID);
+	printf("thread1 %lu and %d \n", posixID, linuxID);
 
 	pthread_mutex_lock(&mFile);
 	pLogFile = fopen(data->fileName, "a");
 	if(pLogFile == NULL)
 		printf("Unable to open file\n");
 
-	fprintf(pLogFile, "Thread 1 is writing to the file\n");
-	fprintf(pLogFile, "POSIX id is %ld and LINUX id is %d\n", posixID, linuxID);
+	fprintf(pLogFile, "Thread1 is writing to the file\n");
+	fprintf(pLogFile, "Thread1 Starting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
+	fprintf(pLogFile, "Thread1 POSIX id is %lu and LINUX id is %d\n", posixID, linuxID);
+
+	FILE *pStat = fopen("/proc/stat", "r");
+
+
+	time(&currentTime);
+	cTime = gmtime(&currentTime);
+	fprintf(pLogFile, "Thread1 Exiting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
 
 	fclose(pLogFile);
 
+	pthread_mutex_unlock(&mTime);
 	pthread_mutex_unlock(&mFile);
 
 	return NULL;
@@ -129,6 +170,7 @@ int main(int argc, char **argv){
 	pthread_t threads[NUMBER_OF_THREADS];
 
 	pthread_mutex_init(&mFile, NULL);
+	pthread_mutex_init(&mTime, NULL);
 
 	int index = 0;
 
@@ -141,34 +183,30 @@ int main(int argc, char **argv){
 
 	/*Create two threads data*/
 	for(index = 0; index < NUMBER_OF_THREADS; ++index){
-
 		threadData[index].fileName = logFile;
-		threadData[index].threadId = index;
-
-		//printf("%d and %s\n", threadData[index].threadId, threadData[index].fileName);
 	}
 
 	/*Create two threads*/
 	if(pthread_create(&threads[0], NULL, funThread0, (void*)&threadData[0]))
 		printf("Error while spawning a thread\n");
-	sleep(1);
+	//sleep(1);
 	if(pthread_create(&threads[1], NULL, funThread1, (void*)&threadData[1]))
 		printf("Error while spawning a thread\n");
-	sleep(1);
-	
+	//sleep(1);
+
 	/*Main threads waits for two child threads to complete the execution*/
 	for(index = 0; index < NUMBER_OF_THREADS; index++){
 
-		pthread_join(threads[index], NULL);
+		if(pthread_join(threads[index], NULL)){
+			printf("Error in pthread join");
+		}
 	}
 
-	
-
-
-/*	FILE *pLogFile = fopen(logFile, "w");
+	/*	FILE *pLogFile = fopen(logFile, "w");
 	if(pLogFile == NULL)
 		printf("Unable to open a file\n");
-*/
+	 */
+
 	return 0;
 
 }
