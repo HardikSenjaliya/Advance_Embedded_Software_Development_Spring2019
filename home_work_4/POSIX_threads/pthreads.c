ed4f0fb75bb372a61b8652fa3289818acdc8a1a3
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-02-26 05:19:06 -0700

    added functionality to handle USR1 and USR2 linux signals

--- home_work_4/POSIX_threads/pthreads.c
+++ home_work_4/POSIX_threads/pthreads.c
@@ -5,7 +5,6 @@
  *      Author: hardyk
  */
 
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,7 +12,7 @@
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <sys/signal.h>
+#include <signal.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/syscall.h>
@@ -28,20 +27,24 @@
 
 /*Function prototype*/
 void readMapChars(FILE *fp, int array[SIZE_ROW][SIZE_COL]);
-void createPeriodicThread(void);
+int createPeriodicThread(void);
 void timerHandler(union sigval);
+int setupSigactionHandler(void);
+void signalHandler(int, siginfo_t *, void *);
+int setSignalMask(void);
 
 /*Mutex and semaphore for synchronized operation to write into logfile*/
 pthread_mutex_t mFile, mTime;
 sem_t sFile;
 
+/*Global varibale to handle USR1 and USR2 signal*/
+int usrTwoSignal = 0, usrOneSignal = 0;
 
 /*Structure to pass data for thread function*/
-typedef struct threadData
-{
-	char *fileName;		/*log file name*/
+typedef struct threadData {
+	char *fileName; /*log file name*/
 
-}threadData_t;
+} threadData_t;
 
 /*global: file pointer for a logfile*/
 FILE *pLogFile;
@@ -50,16 +53,60 @@ FILE *pLogFile;
 time_t currentTime;
 struct tm *cTime;
 
-
-void timerHandler(union sigval arg){
-
-	printf("Inside timer handler\n");
+void timerHandler(union sigval arg) {
 	/*timer expired, run thread1*/
 	sem_post(&sFile);
 }
 
+int setSignalMask(){
 
-void createPeriodicThread(){
+	sigset_t signalSet;
+	sigemptyset(&signalSet);
+	sigaddset(&signalSet, SIGUSR1);
+	sigaddset(&signalSet, SIGUSR2);
+
+	pthread_sigmask(SIG_BLOCK, &signalSet, NULL);
+
+	return 0;
+
+}
+
+
+void signalHandler(int signal, siginfo_t *siginfo, void *ucontext){
+
+	if(signal == SIGUSR1){
+		usrOneSignal = 1;
+	}
+	else if(signal == SIGUSR2){
+		usrTwoSignal = 1;
+	}
+	else{
+		exit(1);
+	}
+
+}
+
+int setupSigactionHandler(){
+
+	struct sigaction act;
+
+	memset(&act, 0, sizeof(act));
+
+	act.sa_sigaction = signalHandler;
+	act.sa_flags = SA_SIGINFO;
+
+	if((sigaction(SIGUSR1, &act, NULL)) < 0){
+			printf("ERROR - Registering USR1\n");
+			return 1;
+	}
+	if((sigaction(SIGUSR2, &act, NULL)) < 0){
+			printf("ERROR - Registering USR2\n");
+			return 1;
+	}
+	return 0;
+}
+
+int createPeriodicThread() {
 
 	timer_t idTimer;
 	struct itimerspec ts;
@@ -76,46 +123,54 @@ void createPeriodicThread(){
 	 * period of 100ms*/
 	long long int nanoSec = MSEC_100 * MSEC_TO_NSEC;
 	ts.it_value.tv_sec = nanoSec / NSEC_TO_SEC;
-	ts.it_value.tv_nsec =  nanoSec % NSEC_TO_SEC;
+	ts.it_value.tv_nsec = nanoSec % NSEC_TO_SEC;
 	ts.it_interval.tv_sec = ts.it_value.tv_sec;
 	ts.it_interval.tv_nsec = ts.it_value.tv_nsec;
 
 	/*create a new timer*/
-	if((timer_create(CLOCK_MONOTONIC, &se, &idTimer)) == -1)
+	if ((timer_create(CLOCK_MONOTONIC, &se, &idTimer)) == -1){
 		printf("ERROR - creating a new timer failed\n");
-
+		return 1;
+	}
 	/*Set the timer to be periodic at 100msec*/
-	if((timer_settime(idTimer, 0, &ts, 0)) == -1)
+	if ((timer_settime(idTimer, 0, &ts, 0)) == -1){
 		printf("ERROR - setting up a period of the timer\n");
+		return 1;
+	}
+
+	return 0;
 }
 
-
-void readMapChars(FILE *fp, int array[SIZE_ROW][SIZE_COL]){
+void readMapChars(FILE *fp, int array[SIZE_ROW][SIZE_COL]) {
 
 	char c;
-	while((c = fgetc(fp)) != EOF  && !feof(fp)){
-		if((c >= 65) || (c <= 90))
+	while ((c = fgetc(fp)) != EOF && !feof(fp)) {
+		if ((c >= 65) || (c <= 90))
 			c = tolower(c);
 
-		array[(int)c][0] = (int)c;
-		array[(int)c][1] += 1;
+		array[(int) c][0] = (int) c;
+		array[(int) c][1] += 1;
 	}
 }
 
-void *funThread0(void *arg){
+void *funThread0(void *arg) {
+
+	/*Set signal mask*/
+	if(setSignalMask())
+		exit(1);
 
 	/*Get an exclusive access of a global clock/time variabels*/
 	pthread_mutex_lock(&mTime);
 	time(&currentTime);
 	cTime = gmtime(&currentTime);
 
-	threadData_t *data = (threadData_t*)(arg);
+	threadData_t *data = (threadData_t*) (arg);
 	int mapChars[SIZE_ROW][SIZE_COL];
 	int indexR = 0, indexC = 0;
 
 	/*Initialize the array to 0*/
-	for(indexR = 0; indexR < SIZE_ROW; indexR++){
-		for(indexC = 0; indexC < SIZE_COL; indexC++)
+	for (indexR = 0; indexR < SIZE_ROW; indexR++) {
+		for (indexC = 0; indexC < SIZE_COL; indexC++)
 			mapChars[indexR][indexC] = 0;
 	}
 
@@ -130,17 +185,19 @@ void *funThread0(void *arg){
 
 	/*Open the logfile*/
 	pLogFile = fopen(data->fileName, "a");
-	if(pLogFile == NULL)
+	if (pLogFile == NULL)
 		printf("Unable to open log file\n");
 
 	/*Write to the log file*/
 	fprintf(pLogFile, "Thread0 is writing to the file\n");
-	fprintf(pLogFile, "Thread0 Starting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
-	fprintf(pLogFile, "Thread0 POSIX id is %lu and LINUX id is %d\n", posixID, linuxID);
+	fprintf(pLogFile, "Thread0 Starting time : %02d:%02d:%02d\n",
+			(cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
+	fprintf(pLogFile, "Thread0 POSIX id is %lu and LINUX id is %d\n", posixID,
+			linuxID);
 
 	/*Open the test file to read data*/
 	FILE *pTestFile = fopen("/usr/bin/gdb.txt", "r");
-	if(pTestFile == NULL)
+	if (pTestFile == NULL)
 		printf("Unable to open test file\n");
 
 	/*Map all the characters in the file*/
@@ -150,15 +207,17 @@ void *funThread0(void *arg){
 	fclose(pTestFile);
 
 	/*Write all the characters with frequency less than 100 to the logfile*/
-	for(indexR = 0; indexR < SIZE_ROW; indexR++){
+	for (indexR = 0; indexR < SIZE_ROW; indexR++) {
 		//printf("%c and %d\n", (char)mapChars[indexR][0], mapChars[indexR][1]);
-		if(mapChars[indexR][1] < 100 && mapChars[indexR][1] > 0)
-			fprintf(pLogFile,"%c\t %d\t\n", mapChars[indexR][0], mapChars[indexR][1]);
+		if (mapChars[indexR][1] < 100 && mapChars[indexR][1] > 0)
+			fprintf(pLogFile, "%c\t %d\t\n", mapChars[indexR][0],
+					mapChars[indexR][1]);
 	}
 
 	time(&currentTime);
 	cTime = gmtime(&currentTime);
-	fprintf(pLogFile, "Thread0 Exiting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
+	fprintf(pLogFile, "Thread0 Task Completed \n Thread0 Exiting time : %02d:%02d:%02d\n",
+			(cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
 
 	fclose(pLogFile);
 
@@ -168,118 +227,140 @@ void *funThread0(void *arg){
 	return NULL;
 }
 
-
-void *funThread1(void *arg){
+void *funThread1(void *arg) {
 
 	long int cpuStat[10];
 	char name[5];
 
+	//printf("Entered DEBUG\n");
 	/*Get an exclusive access of a global clock/time variabels*/
 	pthread_mutex_lock(&mTime);
 	time(&currentTime);
 	cTime = gmtime(&currentTime);
-
-	threadData_t *data = (threadData_t*)(arg);
+	pthread_mutex_unlock(&mTime);
+	//printf("released mTime1DEBUG\n");
+	threadData_t *data = (threadData_t*) (arg);
 
 	pthread_t posixID = pthread_self();
 	pid_t linuxID = syscall(SYS_gettid);
 
 	printf("thread1 %lu and %d \n", posixID, linuxID);
 
-	/*Wait till timer expires*/
-	sem_wait(&sFile);
+	while (!usrTwoSignal) {
 
-	printf("semaphore released\n");
+		/*Wait till timer expires*/
+		sem_wait(&sFile);
+		//printf("Timer expired DEBUG\n");
+		/*get the exclusive access to logfile*/
+		pthread_mutex_lock(&mFile);
+		//printf("aquired mFile DEBUG\n");
+		pLogFile = fopen(data->fileName, "a");
+		if (pLogFile == NULL)
+			printf("Unable to open file\n");
 
-	/*get the exclusive access to logfile*/
-	pthread_mutex_lock(&mFile);
-	pLogFile = fopen(data->fileName, "a");
-	if(pLogFile == NULL)
-		printf("Unable to open file\n");
+		fprintf(pLogFile, "Thread1 is writing to the file\n");
+		fprintf(pLogFile, "Thread1 Starting time : %02d:%02d:%02d\n",
+				(cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
+		fprintf(pLogFile, "Thread1 POSIX id is %lu and LINUX id is %d\n",
+				posixID, linuxID);
 
-	fprintf(pLogFile, "Thread1 is writing to the file\n");
-	fprintf(pLogFile, "Thread1 Starting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
-	fprintf(pLogFile, "Thread1 POSIX id is %lu and LINUX id is %d\n", posixID, linuxID);
+		/*open stat file to read cpu usage and log the usage into logfile*/
+		FILE *pStatFile = fopen("/proc/stat", "r");
+		fscanf(pStatFile, "%s %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld", name,
+				&cpuStat[0], &cpuStat[1], &cpuStat[2], &cpuStat[3], &cpuStat[4],
+				&cpuStat[5], &cpuStat[6], &cpuStat[7], &cpuStat[8],
+				&cpuStat[9]);
+		fclose(pStatFile);
+		fprintf(pLogFile,
+				"CPU Utilization\n%s\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\n",
+				name, cpuStat[0], cpuStat[1], cpuStat[2], cpuStat[3],
+				cpuStat[4], cpuStat[5], cpuStat[6], cpuStat[7], cpuStat[8],
+				cpuStat[9]);
 
-	/*open stat file to read cpu usage and log the usage into logfile*/
-	FILE *pStatFile = fopen("/proc/stat", "r");
-	fscanf(pStatFile, "%s %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld",
-						name, &cpuStat[0], &cpuStat[1], &cpuStat[2], &cpuStat[3], &cpuStat[4],
-						&cpuStat[5], &cpuStat[6], &cpuStat[7], &cpuStat[8], &cpuStat[9]);
-	fclose(pStatFile);
-	fprintf(pLogFile, "CPU Utilization\n %s\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\t%ld\n",
-						name, cpuStat[0], cpuStat[1], cpuStat[2], cpuStat[3], cpuStat[4],
-						cpuStat[5], cpuStat[6], cpuStat[7], cpuStat[8], cpuStat[9]);
+		fclose(pLogFile);
+		pthread_mutex_unlock(&mFile);
+		//printf("released mFile DEBUG \n");
+	}
 
+	/*Signal USR2 received, exit and write to the log file*/
+	printf("While 1 completed\n");
+	pthread_mutex_lock(&mTime);
 	time(&currentTime);
 	cTime = gmtime(&currentTime);
-	fprintf(pLogFile, "Thread1 Exiting time : %02d:%02d:%02d\n", (cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
-
+	pthread_mutex_lock(&mFile);
+	pLogFile = fopen(data->fileName, "a");
+	if (pLogFile == NULL)
+		printf("Unable to open file\n");
+	fprintf(pLogFile, "Thread1 USR2 Signal Received \nThread1 Exiting time : %02d:%02d:%02d\n",
+			(cTime->tm_hour), cTime->tm_min, cTime->tm_sec);
 	fclose(pLogFile);
-
 	pthread_mutex_unlock(&mTime);
 	pthread_mutex_unlock(&mFile);
 
 	return NULL;
 }
 
-
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
 
 	threadData_t threadData[NUMBER_OF_THREADS];
 	pthread_t threads[NUMBER_OF_THREADS];
 
 	/*Initialize mutex and semaphore*/
-	if(pthread_mutex_init(&mFile, NULL))
+	if (pthread_mutex_init(&mFile, NULL))
 		printf("ERROR - initializing mFile mutex\n");
 
-	if(pthread_mutex_init(&mTime, NULL))
+	if (pthread_mutex_init(&mTime, NULL))
 		printf("ERROR - initializing mTime mutex\n");
 
-	if((sem_init(&sFile, 0, 0)) == -1)
+	if ((sem_init(&sFile, 0, 0)) == -1)
 		printf("ERROR - initializing a sFile semaphore\n");
-
 
 	int index = 0;
 
 	/*Error if file name is not provided at the command line*/
-	if(argc < 2 )
-		printf("Invalid command line argument/No command line argument\nUsage : make run fileName=yourfilename.txt \n");
+	if (argc < 2)
+		printf(
+				"Invalid command line argument/No command line argument\nUsage : make run fileName=yourfilename.txt \n");
 
 	/*store the file name from the command line*/
 	char *logFile = argv[1];
 
 	/*Create two threads data*/
-	for(index = 0; index < NUMBER_OF_THREADS; ++index){
+	for (index = 0; index < NUMBER_OF_THREADS; ++index) {
 		threadData[index].fileName = logFile;
 	}
 
 	/*Create and set the timer*/
-	createPeriodicThread();
+	if(createPeriodicThread())
+		exit(1);
+
+	/*Set sigaction to handler USR1 and USR2*/
+	if(setupSigactionHandler())
+		exit(1);
 
 	/*Create two threads*/
-	if(pthread_create(&threads[0], NULL, funThread0, (void*)&threadData[0]))
+	if (pthread_create(&threads[0], NULL, funThread0, (void*) &threadData[0]))
 		printf("ERROR - spawning a thread0\n");
 
-	if(pthread_create(&threads[1], NULL, funThread1, (void*)&threadData[1]))
+	if (pthread_create(&threads[1], NULL, funThread1, (void*) &threadData[1]))
 		printf("ERROR - spawning a thread1\n");
 
-
 	/*Main threads waits for two child threads to complete the execution*/
-	for(index = 0; index < NUMBER_OF_THREADS; index++){
+	for (index = 0; index < NUMBER_OF_THREADS; index++) {
 
-		if(pthread_join(threads[index], NULL)){
+		if (pthread_join(threads[index], NULL)) {
 			printf("ERROR - pthread join\n");
 		}
 	}
 
-	/*	FILE *pLogFile = fopen(logFile, "w");
+	pthread_mutex_lock(&mFile);
+	FILE *pLogFile = fopen(logFile, "a");
 	if(pLogFile == NULL)
 		printf("Unable to open a file\n");
-	 */
-
+	fprintf(pLogFile, "Main thread - Exiting\n");
+	fclose(pLogFile);
+	pthread_mutex_unlock(&mFile);
 	return 0;
 
 }
-
 
