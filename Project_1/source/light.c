9525eb067f0e060d63825bf78267998a816dbaab
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-03-13 07:13:44 -0600

    adding a message queue to communicate with logger task

--- Project_1/source/light.c
+++ Project_1/source/light.c
@@ -7,6 +7,9 @@
 
 #include "../include/light.h"
 
+#define Q_NAME				"/qLightToLog"
+#define THREAD_NAME			"LIGHT_SENSOR"
+
 
 /**
  * @brief this function is the thread function for the thread light_sensor
@@ -14,6 +17,18 @@
  * @return NULL
  */
 void *run_light_sensor(void *params){
+
+	mqd_t qDes = create_posix_mq(Q_NAME);
+
+	log_message_t msg;
+	msg.log_level = 0;
+	strcpy(msg.message, "Hello From Light Sensor task");
+	strcpy(msg.thread_name, THREAD_NAME);
+	clock_gettime(CLOCK_MONOTONIC, &msg.time_stamp);
+
+	while(1){
+		send_message(qDes, msg);
+	}
 
 	return NULL;
 }
