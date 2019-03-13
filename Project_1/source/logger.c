4d6669f84a2270fa93677e56b09148432d8f0c4c
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-03-13 07:14:19 -0600

    adding functionality to read messages from light and temperature threads

--- Project_1/source/logger.c
+++ Project_1/source/logger.c
@@ -8,6 +8,10 @@
 #include "../include/logger.h"
 #define NSEC_PER_SEC 			(1000000000)
 #define Q_NAME_MAIN				"/qMaintoLog"
+#define Q_NAME_LIGHT			"/qLightToLog"
+#define Q_NAME_TEMP				"/qTemperatureToLog"
+
+pthread_mutex_t fileMutex = PTHREAD_MUTEX_INITIALIZER;
 
 /**
  * @brief this function logs informative messages from the logger thread into the logfile
@@ -18,14 +22,11 @@
  * @param message    message sent by the thread
  * @return void
  */
-void log_message(FILE *fp, long int time_stamp, unsigned int thread_id, int log_level, char *message){
+void log_message(FILE *fp, long int time_stamp, char *thread_name,
+		int log_level, char *message) {
 
-	fprintf(fp, "[%ld] %u %s\n", time_stamp, thread_id, message);
-
-	fclose(fp);
+	fprintf(fp, "[%ld] %s %s\n", time_stamp, thread_name, message);
 }
-
-
 
 /**
  * @brief this function is the thread function for the thread logger
@@ -38,6 +39,8 @@ void *run_logger(void *params) {
 	FILE *p_logfile = fopen("logfile.txt", "a");
 
 	mqd_t qDesMain = create_posix_mq(Q_NAME_MAIN);
+	mqd_t qDesLight = create_posix_mq(Q_NAME_LIGHT);
+	mqd_t qDesTemp = create_posix_mq(Q_NAME_TEMP);
 
 	int bytes_read = 0;
 	log_message_t read_message;
@@ -49,14 +52,50 @@ void *run_logger(void *params) {
 
 		if (bytes_read < 0) {
 			perror("ERROR: mq_receive");
+		} else {
+			pthread_mutex_lock(&fileMutex);
+
+			log_message(p_logfile,
+					(read_message.time_stamp.tv_sec) * NSEC_PER_SEC
+							+ (read_message.time_stamp.tv_nsec),
+					read_message.thread_name, read_message.log_level,
+					read_message.message);
+
+			pthread_mutex_unlock(&fileMutex);
 		}
 
-		log_message(p_logfile,
-				(read_message.time_stamp.tv_sec) * NSEC_PER_SEC
-						+ (read_message.time_stamp.tv_nsec),
-				read_message.thread_id, read_message.log_level,
-				read_message.message);
+		bytes_read = mq_receive(qDesLight, (char*) &read_message,
+				sizeof(read_message), 0);
+
+		if (bytes_read < 0) {
+			perror("ERROR: mq_receive");
+		} else {
+			pthread_mutex_lock(&fileMutex);
+			log_message(p_logfile,
+					(read_message.time_stamp.tv_sec) * NSEC_PER_SEC
+							+ (read_message.time_stamp.tv_nsec),
+					read_message.thread_name, read_message.log_level,
+					read_message.message);
+			pthread_mutex_unlock(&fileMutex);
+		}
+
+		bytes_read = mq_receive(qDesTemp, (char*) &read_message,
+				sizeof(read_message), 0);
+
+		if (bytes_read < 0) {
+			perror("ERROR: mq_receive");
+		} else {
+			pthread_mutex_lock(&fileMutex);
+			log_message(p_logfile,
+					(read_message.time_stamp.tv_sec) * NSEC_PER_SEC
+							+ (read_message.time_stamp.tv_nsec),
+					read_message.thread_name, read_message.log_level,
+					read_message.message);
+			pthread_mutex_unlock(&fileMutex);
+		}
 	}
+
+	fclose(p_logfile);
 
 	return NULL;
 }
