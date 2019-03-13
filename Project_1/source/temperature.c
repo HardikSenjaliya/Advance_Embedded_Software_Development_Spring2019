69bacb91ca0678254bdeb54c0780457287b23da1
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-03-13 07:13:07 -0600

    adding a message queue to communicate with logger task

--- Project_1/source/temperature.c
+++ Project_1/source/temperature.c
@@ -7,12 +7,27 @@
 
 #include "../include/temperature.h"
 
+#define Q_NAME				"/qTemperatureToLog"
+#define THREAD_NAME			"TEMP_SENSOR"
+
 /**
  * @brief this function is the thread function for the thread temperature_sensor
  * @param params
  * @return NULL
  */
-void *run_temperature_sensor(void *params){
+void *run_temperature_sensor(void *params) {
+
+	mqd_t qDes = create_posix_mq(Q_NAME);
+
+	log_message_t msg;
+	msg.log_level = 0;
+	strcpy(msg.message, "Hello From Temp Sensor task");
+	strcpy(msg.thread_name, THREAD_NAME);
+	clock_gettime(CLOCK_MONOTONIC, &msg.time_stamp);
+
+	while (1) {
+		send_message(qDes, msg);
+	}
 
 	return NULL;
 
