cd96c9cc26176b2f1201f753f9dfb8ff54aa7d1f
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-03-13 04:51:02 -0600

    added Message queue for communication with logger task

--- Project_1/include/main.h
+++ Project_1/include/main.h
@@ -13,10 +13,16 @@
 #include <stdint.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <sys/syscall.h>
 
 #include "../include/utils.h"
 #include "../include/light.h"
 #include "../include/temperature.h"
 #include "../include/logger.h"
+
+
+
+
 
 #endif /* SOURCE_MAIN_H_ */
--- Project_1/source/main.c
+++ Project_1/source/main.c
@@ -8,6 +8,10 @@
 
 #include "../include/main.h"
 
+
+#define Q_NAME				"/qMaintoLog"
+
+
 /**
  * @brief main function
  * @return 0
@@ -15,25 +19,41 @@
 
 int main(void){
 
-	pthread_t light_sensor, temperatture_sensor, logger;
+	log_message_t message;
+
+	pthread_t light_sensor, temperature_sensor, logger;
+
+	mqd_t qDes = create_posix_mq(Q_NAME);
+
+		strcpy(message.message, "ERROR: spawning light sensor thread");
+		message.thread_id = syscall(SYS_gettid);
+		clock_gettime(CLOCK_MONOTONIC, &(message.time_stamp));
+
+		send_message(qDes, message);
 
 	/*Spwan new threads*/
 
 	/*1. light sensor thread*/
-	if(pthread_create(&light_sensor, NULL, run_light_sensor, NULL))
-		printf("ERROR: spawning light sensor thread\n");
+	if(pthread_create(&light_sensor, NULL, run_light_sensor, NULL)){
+
+		strcpy(message.message, "ERROR: spawning light sensor thread");
+		message.thread_id = syscall(SYS_gettid);
+		clock_gettime(CLOCK_MONOTONIC, &(message.time_stamp));
+
+		send_message(qDes, message);
+	}
 
 	/*2. temperature sensor task*/
-	if(pthread_create(&light_sensor, NULL, run_temperature_sensor, NULL))
+	if(pthread_create(&temperature_sensor, NULL, run_temperature_sensor, NULL))
 		printf("ERROR: spawning temperature sensor thread\n");
 
 	/*3. Logger task*/
-	if(pthread_create(&light_sensor, NULL, run_logger, NULL))
+	if(pthread_create(&logger, NULL, run_logger, NULL))
 		printf("ERROR: spawning logger thread\n");
 
 
 	pthread_join(light_sensor, NULL);
-	pthread_join(temperatture_sensor, NULL);
+	pthread_join(temperature_sensor, NULL);
 	pthread_join(logger, NULL);
 
 
