2529e77f64c0bee164fafe5887adec649639160f
Hardik Senjaliya <hardyksenjaliya243@gmail.com>
2019-03-26 07:50:19 -0600

    adding i2c driver files for temperature sensor

---
+++ Project_1/include/TMP_102_temp_sensor.h
@@ -0,0 +1,77 @@
+/*
+ * TMP_102_temp_sensor.h
+ *
+ *  Created on: Mar 25, 2019
+ *      Author: hardyk
+ */
+
+#ifndef INCLUDE_TMP_102_TEMP_SENSOR_H_
+#define INCLUDE_TMP_102_TEMP_SENSOR_H_
+
+#include "../include/utils.h"
+
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+
+#define NBYTES_1							(0x01)
+#define NBYTES_2							(0x02)
+#define NBYTES_3							(0x03)
+#define NEGATIVE_CHECK						(0x10000000)
+
+#define TEMPERATURE_REGISTER 				(0x00)
+#define CONFIGURATION_REGISTER				(0x01)
+#define TLOW_REGISTER						(0x02)
+#define THIGH_REGISTER						(0x03)
+
+#define CR_EXTENDED_MODE_MASK				(0x12)
+#define CR_ALERT_PIN_MASK					(0x13)
+#define CR_CONVERSION_RATE_MASK				(0x14)
+#define CR_RESOLUTION_MASK					(0x05)
+#define CR_SHUTDOWN_MODE_MASK				(0x00)
+#define CR_THERMOSTAT_MASK					(0X01)
+#define CR_FAULT_BIT_MASK					(0x03)
+
+#define FAULTS_ONE							(0x00)
+#define FAULTS_TWO							(0x01)
+#define FAULTS_FOUR							(0x02)
+#define FAULTS_SIX							(0x03)
+
+#define CONVERSION_RATE_QUARTER_HZ			(0x00)
+#define CONVERSION_RATE_ONE_HZ				(0x01)
+#define CONVERSION_RATE_DEFAULT				(0x02)
+#define CONVERSION_RATE_EIGHT_HZ			(0x03)
+
+#define COMPARATOR_MODE						(0x00)
+#define INTERRUPT_MODE						(0x01)
+
+#define DISBALE_SHUTDOWN_MODE				(0X00)
+#define ENABLE_SHUTDOWN_MODE				(0x01)
+
+#define NORMAL_MODE_OPERATION				(0x00)
+#define EXTENDED_MODE_OPERATION				(0x01)
+
+
+/*Function Prototypes*/
+void init_temp_sensor(void);
+double read_temperature_register(int i2c_fd);
+double binary_to_decimal(int *binary_data);
+int write_pointer_register(int i2c_fd, int reg);
+int read_tlow_register(int i2c_fd);
+int read_thigh_register(int i2c_fd);
+int read_config_register(int i2c_fd);
+int8_t write_config_register(int i2c_fd, int16_t data);
+int8_t configure_shutdown_mode(int i2c_fd, bool required_mode);
+int8_t configure_sensor_resolution(int i2c_fd);
+int8_t configure_EM_operation(int i2c_fd, bool required_operation);
+int8_t configure_conversion_rate(int i2c_fd, int8_t required_rate);
+int8_t configure_thermostate_mode(int i2c_fd, bool required_mode);
+int8_t configure_fault_bits(int i2c_fd, int8_t required_faults);
+
+
+
+
+#endif /* INCLUDE_TMP_102_TEMP_SENSOR_H_ */
---
+++ Project_1/source/TMP_102_temp_sensor.c
@@ -0,0 +1,300 @@
+/*
+ * TMP_102_temp_sensor.c
+ *
+ *  Created on: Mar 25, 2019
+ *      Author: hardyk
+ */
+
+#include "../include/TMP_102_temp_sensor.h"
+
+#define I2C_SLAVE_ADDRESS				(0x00)
+#define CONFIG_REG_DEFAULT				(0xA060) /*TODO check for the correctness of the MSB and LSB order*/
+
+/**
+ * @brief this function sets the mode of opearation either comparator or interrupt mode
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param required_mode
+ * @return no of bytes written
+ */
+int8_t configure_thermostate_mode(int i2c_fd, bool required_mode) {
+
+	int16_t current_config = 0;
+	int ret = 0;
+
+	current_config = read_config_register(i2c_fd);
+
+	if (required_mode == INTERRUPT_MODE) {
+		current_config |= (1 << CR_THERMOSTAT_MASK);
+	} else {
+		current_config &= ~(1 << CR_THERMOSTAT_MASK);
+	}
+
+	ret = write_config_register(i2c_fd, current_config);
+	if (ret < 0) {
+		perror("ERROR: while setting shutdown mode");
+	}
+
+	return ret;
+}
+
+/**
+ * @brief this function sets the requied number of consecutive faults bits
+ * 			configurable into four modes - 1, 2, 4 and 6
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param required_faults required number of faults bits
+ * @return no of bytes written
+ */
+int8_t configure_fault_bits(int i2c_fd, int8_t required_faults) {
+
+	int16_t current_config = 0;
+	int ret = 0;
+
+	current_config = read_config_register(i2c_fd);
+
+	current_config |= (required_faults << CR_FAULT_BIT_MASK);
+
+	ret = write_config_register(i2c_fd, current_config);
+	if (ret < 0) {
+		perror("ERROR: while setting shutdown mode");
+	}
+
+	return ret;
+
+}
+/**
+ * @brief this function sets the conversion rate of the sensor
+ *		 	configurable into four modes - 0.25Hz, 1 Hz, 4 Hz, 8 Hz
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param required_rate conversion rate to be configured
+ * @return no of bytes written
+ */
+int8_t configure_conversion_rate(int i2c_fd, int8_t required_rate) {
+
+	int16_t current_config = 0;
+	int ret = 0;
+
+	current_config = read_config_register(i2c_fd);
+
+	current_config |= (required_rate << CR_CONVERSION_RATE_MASK);
+
+	ret = write_config_register(i2c_fd, current_config);
+	if (ret < 0) {
+		perror("ERROR: while setting shutdown mode");
+	}
+
+	return ret;
+
+}
+
+/**
+ * @brief this function configures extended mode of operation of the sensor
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param required_operation 1 = extended mode, 0 = normal mode
+ * @return no of bytes written
+ */
+int8_t configure_EM_operation(int i2c_fd, bool required_operation) {
+
+	int16_t current_config = 0;
+	int ret = 0;
+
+	current_config = read_config_register(i2c_fd);
+
+	if (required_operation == EXTENDED_MODE_OPERATION) {
+		current_config |= (1 << CR_EXTENDED_MODE_MASK);
+	} else {
+		current_config &= ~(1 << CR_EXTENDED_MODE_MASK);
+	}
+
+	ret = write_config_register(i2c_fd, current_config);
+	if (ret < 0) {
+		perror("ERROR: while setting shutdown mode");
+	}
+
+	return ret;
+}
+
+/**
+ * @brief this function configures/reads the resolution of the sensor
+ * sensor resolution is read only setting.
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param requied_resolution
+ * @return read resolution value
+ */
+
+int8_t configure_sensor_resolution(int i2c_fd) {
+
+	int16_t read_data = 0;
+	int8_t resolution = 0;
+	int8_t msb = 0, lsb = 0;
+
+	read_data = read_config_register(i2c_fd);
+
+	msb = (read_data >> 8);
+	lsb = (read_data & 0xFF);
+
+	resolution = (msb & 0x60);
+
+	return resolution;
+}
+
+/**
+ * @brief this function enables/disables sensor's mode of operation
+ * 			1 = enables shutdown mode, 0 disables shutdown mode
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param required_mode
+ * @return no of bytes written
+ */
+int8_t configure_shutdown_mode(int i2c_fd, bool required_mode) {
+
+	int16_t current_config = 0;
+	int ret = 0;
+
+	current_config = read_config_register(i2c_fd);
+
+	current_config |= (required_mode << CR_SHUTDOWN_MODE_MASK);
+
+	ret = write_config_register(i2c_fd, current_config);
+	if (ret < 0) {
+		perror("ERROR: while setting shutdown mode");
+	}
+
+	return ret;
+}
+
+int8_t write_config_register(int i2c_fd, int16_t data) {
+
+	int8_t ret = 0;
+
+	if (write_pointer_register(i2c_fd, CONFIGURATION_REGISTER)) {
+		perror("ERROR: while writing to the pointer register\n");
+	}
+
+	int8_t write_buffer[NBYTES_3];
+	write_buffer[0] = CONFIGURATION_REGISTER;
+	write_buffer[1] = (data >> 8);
+	write_buffer[2] = (data & 0xFF);
+
+	ret = write(i2c_fd, &write_buffer, NBYTES_3);
+	if (ret < 0) {
+		perror("ERROR: while writing data to configuration register\n");
+	}
+
+	return ret;
+}
+
+/**
+ * @brief this function reads the Configuration Register
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @return read value
+ */
+int16_t read_config_register(int i2c_fd) {
+
+	int16_t read_buffer = 0;
+	int read_bytes = 0;
+
+	if (write_pointer_register(i2c_fd, CONFIGURATION_REGISTER)) {
+		ERROR_STDOUT("ERROR: while writing to the pointer register\n");
+	}
+
+	read_bytes = read(i2c_fd, &read_buffer, NBYTES_2);
+	if (read_bytes < 0) {
+		perror("ERROR: reading configuration register");
+	}
+
+	return read_buffer;
+}
+
+/**
+ * @brief this function converts digital(binary) temp data read from the register
+ * into decimal value
+ * @param binary_data
+ * @return decimal temperature value
+ */
+double binary_to_decimal(int8_t *binary_data) {
+
+	double decimal_temp = 0;
+	int16_t temp = 0;
+
+	int8_t msb_byte = binary_data[0];
+	int8_t lsb_byte = binary_data[1];
+
+	temp = ((((uint16_t) msb_byte) << 4) | (lsb_byte >> 4)) & 0xFFF;
+
+	if (msb_byte & NEGATIVE_CHECK) {
+
+		decimal_temp = ((~temp + 1) * 0.0625) * (-1);
+
+	} else {
+
+		decimal_temp = temp * 0.0625;
+	}
+
+	return decimal_temp;
+}
+/**
+ * @brief this function reads the temperature value in binary format from the
+ * register
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @return temperature in decimal value
+ */
+double read_temperature_register(int i2c_fd) {
+
+	double temp_read = 0;
+
+	int8_t read_buffer[NBYTES_2];
+	int8_t read_bytes = 0;
+
+	if (write_pointer_register(i2c_fd, TEMPERATURE_REGISTER)) {
+		ERROR_STDOUT("ERROR: while writing to the pointer register\n");
+	}
+
+	read_bytes = read(i2c_fd, &read_buffer, NBYTES_2);
+	if (read_bytes < 0) {
+		perror("ERROR: reading temp register");
+	}
+
+	temp_read = binary_to_decimal(&read_buffer);
+
+	return temp_read;
+}
+
+/**
+ * @brief this function writes to the pointer register
+ * @param i2c_fd file descriptor of the opened i2c module
+ * @param reg register to be writtten/read
+ * @return 0 if successful, 1 otherwise
+ */
+int write_pointer_register(int i2c_fd, int reg) {
+
+	int ret = 0;
+	ret = write(i2c_fd, &reg, sizeof(reg));
+
+	if (ret < 0) {
+		perror("write_pointer_register");
+		return 1;
+	}
+
+	return 0;
+}
+
+/**
+ * @brief this function initializes temperature sensor for I2C operations
+ * @return void
+ */
+void init_temp_sensor() {
+
+	int i2c_fd = 0;
+
+	i2c_fd = open("/dev/i2c-2", O_RDWR);
+	if (i2c_fd < 0) {
+		perror("ERROR: i2c_open");
+		exit(1);
+	}
+
+	if (ioctl(i2c_fd, I2C_SLAVE, I2C_SLAVE_ADDRESS) < 0) {
+		perror("ERROR: i2c_ioctl");
+		exit(1);
+	}
+
+}
+
